8c3fa06b300a4f1aef1ef558112dbb4af3a9c147
David Floegel <mail@davidfloegel.com>
2024-02-21 15:54:26 +0000

    fix url

--- src/app/config.js
+++ src/app/config.js
@@ -1,1 +1,4 @@
-export const APP_URL = process.env.VERCEL_URL || 'http://localhost:3000'
+const actualURL = 'https://spotify-listening-party.vercel.app/'
+export const APP_URL = process.env.VERCEL_URL
+    ? actualURL
+    : 'http://localhost:3000'
